b50f2e9f1c21bb0921628d1e317e1868b2b907c4
toufik1928 <tofikbeg777@gmail.com>
2025-06-05 16:32:18 +0530

    updated url

--- frontend/components/AuthForm.jsx
+++ frontend/components/AuthForm.jsx
@@ -26,7 +26,7 @@ const AuthForm = ({ onAuthSuccess }) => {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://tbmill-task1.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
@@ -59,7 +59,7 @@ const AuthForm = ({ onAuthSuccess }) => {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://tbmill-task1.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
